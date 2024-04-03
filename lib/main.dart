ed7fe159925aa8e2b287caa7acbbd098c181d8ba
Susmoy Dutta <108541671+SusmoyDutta@users.noreply.github.com>
2024-04-04 02:36:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:task_manager_project/firebase_options.dart';
 import 'package:task_manager_project/presentation/App.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
+
   runApp(const TaskManager());
 }
 
